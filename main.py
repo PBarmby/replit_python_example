5603d69b2d21deedcc7c365a87dbaf447169cfa8
Pauline Barmby <pbarmby@uwo.ca>
2020-08-27 16:07:37 +0000

    Added plotting code

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-print("hello world")
-for i in range(1,11):
-  print(i*2)
+import matplotlib.pyplot as plt
+import numpy as np
+
+xval = np.arange(0,np.pi*4,np.pi*4/50)
+yval = np.sin(xval)
+#print(xval,yval)
+
+fig, ax = plt.subplots()
+ax.scatter(xval, yval)
+ax.set_xlabel(r'$\theta$ [rad]')
+ax.set_ylabel(r'sin($\theta$)')
+plt.show()
